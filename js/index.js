8fe8f061421fd09764c2aa47969871e9005b7cdb
IAOTS <121063496+IAOTS@users.noreply.github.com>
2022-12-20 11:15:58 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
        var btn = document.getElementById("btn");
        btn.onclick = function() {
-           var password = prompt("Enter the password");
-           if (password == "Hanan") {
+          
                parent.location = 'quiz.html';
-           } else {
-               var msg = document.getElementById("msg");
-               msg.innerHTML = "كلمة المرور غير صحيحة";
-           }
+       
        }
